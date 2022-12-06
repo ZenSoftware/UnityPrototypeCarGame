4bc6b2143059e4f558e9eb5633c47340075368e9
Zen Software <ZenSoftware@users.noreply.github.com>
2022-12-05 23:13:56 -0700

    better camera direction\

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -15,8 +15,13 @@ public class FollowPlayer : MonoBehaviour
 
     void LateUpdate()
     {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         var offset = player.transform.forward * offsetBehind + Vector3.up * offsetAbove;
         transform.position = player.transform.position + offset;
-        transform.rotation = player.transform.rotation;
+
+        // Point the camera at the player
+        var cameraForward = player.transform.position - transform.position;
+        var cameraUp = Vector3.Cross(cameraForward.normalized, player.transform.right);
+        transform.rotation = Quaternion.LookRotation(cameraForward, cameraUp);
     }
 }
